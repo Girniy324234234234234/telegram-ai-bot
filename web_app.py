f8d9d64423ea7228ec1ba2d12843e33e623677f4
Huttager <bogdan200501200@gmail.com>
2026-01-13 12:46:24 +0100

    Refactor web_app.py to use Flask instead of HTTPServer

--- web_app.py
+++ web_app.py
@@ -1,16 +1,14 @@
+from flask import Flask
 import os
-from http.server import BaseHTTPRequestHandler, HTTPServer
 
-PORT = int(os.environ.get("PORT", 8080))
+app = Flask(__name__)
 
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(b"Mini App is alive")
+@app.route("/")
+def home():
+    return "Mini App is alive"
 
 if __name__ == "__main__":
-    server = HTTPServer(("", PORT), Handler)
-    print(f"Web server running on port {PORT}")
-    server.serve_forever()
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8080))
+    )
