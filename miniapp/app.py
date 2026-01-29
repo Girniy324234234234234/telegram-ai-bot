b86f5231023e82987c7786ae504b8034f7cbca33
Huttager <bogdan200501200@gmail.com>
2026-01-29 23:26:38 +0100

    Update app.py

--- miniapp/app.py
+++ miniapp/app.py
@@ -50,5 +50,5 @@ def generate():
         return jsonify({"ok": False, "error": "server error"}), 500
 
 
-if name == "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
